7a7e5b9e7d1a8aad68f8305d061a3d1ccf02f466
Your Name <you@example.com>
2024-03-10 00:34:12 +0900

    ServerAliveInterval=1

--- src/mymlops/startup_logs.py
+++ src/mymlops/startup_logs.py
@@ -8,5 +8,7 @@ def do_startup_logs(instance_name, zone):
         instance_name,
         f'--zone={zone}',
         '--command=sudo journalctl -n 1000 -f -u google-startup-scripts.service',
+        '--',
+        '-o', 'ServerAliveInterval=1',
     ]
     run_redirect(options)
